db0658a1f6b32f1a6eadfa08d7356abcc7799df0
Logan Powell <loganp@tepper.cmu.edu>
2019-04-10 15:29:13 -0400

    revert to old gulp to match boilerplate config

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,39 @@
 var gulp = require('gulp');
-var browserify = require('browserify');
-var babelify = require('babelify');
+var sourcemaps = require('gulp-sourcemaps');
 var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var browserify = require('browserify');
+var watchify = require('watchify');
+var babel = require('babelify');
 
-gulp.task('build', function () {
-    return browserify({entries: './src/index.js', extensions: ['.js'], debug: true})
-        .transform(babelify)
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest('dist'));
-});
+function compile(watch) {
+  var bundler = watchify(browserify('./src/index.js', { debug: true }).transform(babel));
+
+  function rebundle() {
+    bundler.bundle()
+      .on('error', function(err) { console.error(err); this.emit('end'); })
+      .pipe(source('build.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(sourcemaps.write('./'))
+      .pipe(gulp.dest('./build'));
+  }
+
+  if (watch) {
+    bundler.on('update', function() {
+      console.log('-> bundling...');
+      rebundle();
+    });
+  }
+
+  rebundle();
+}
+
+function watch() {
+  return compile(true);
+};
+
+gulp.task('build', function() { return compile(); });
+gulp.task('watch', function() { return watch(); });
+
+gulp.task('default', ['watch']);
